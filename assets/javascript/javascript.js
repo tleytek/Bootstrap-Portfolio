23cafe86538866518a8116e018ace6ad2d4c9e44
Mateus Koehler <mateus@mateuskoehler.com>
2019-10-07 17:05:11 -0400

    active link change on scroll and click

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-  $("#aboutMeButton").click(function(event) {
-    event.preventDefault();
-    $("html,body").animate(
-      {
-        scrollTop: $("#aboutMe").offset().top
-      },
-      800
-    );
+  $(document).on("scroll", onScroll);
+
+  // Trigger Click to go specific section or add active
+  $('a[href^="#"]').on("click", function(e) {
+    $(document).off("scroll");
+
+    $("a").each(function() {
+      $(this).removeClass("active");
+    });
+    $(this).addClass("active");
   });
 });
+
+function onScroll(event) {
+  const scrollPosition = window.scrollY;
+  $("nav .nav-link").each(function() {
+    const currentLink = $(this);
+    const refElement = $(currentLink.attr("href"));
+    if (
+      refElement.position().top <= scrollPosition &&
+      refElement.position().top + refElement.height() > scrollPosition
+    ) {
+      $("nav a").removeClass("active");
+      currentLink.addClass("active");
+    } else {
+      currentLink.removeClass("active");
+    }
+  });
+}
